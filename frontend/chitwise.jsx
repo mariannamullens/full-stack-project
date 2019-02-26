ee7b2a045f796f73e3404e8da6ec4f692c879464
marianna mullens <mariannamullens@gmail.com>
2019-02-26 14:12:50 -0800

    Add session API util

--- frontend/chitwise.jsx
+++ frontend/chitwise.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { login, logout, signup } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+
+  // TOFIX: TEST
+  // window.login = login;
+  // window.logout = logout;
+  // window.signup = signup;
+  // testing
+
   ReactDOM.render(<h1>Welcome to the party, pal</h1>, root)
 });
