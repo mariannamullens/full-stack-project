2217ea4c8631abc07b03ac2f223952640c41e7ab
marianna mullens <mariannamullens@gmail.com>
2019-02-26 20:49:49 -0800

    Add header styling

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -20,11 +20,12 @@ class Header extends React.Component {
     const { currentUser, logout, isLoggedOut } = this.props;
     const notLoggedInLinks =
     <div className="not-logged-in">
-      <button className="login" onClick={this.showLogin}>Log in</button>
+      <button className="login-logout" onClick={this.showLogin}>Log in</button>
+      <span>or</span>
       <Link className="signup" to="">Sign up</Link>
     </div>
 
-    const loggedInLinks = <button onClick={logout}>Logout</button>
+    const loggedInLinks = <button className="login-logout" onClick={logout}>Logout</button>
 
     return(
       <header className="header">
