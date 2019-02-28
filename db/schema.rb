556880128f291b2e199cb92a861d9fe035f67fad
marianna mullens <mariannamullens@gmail.com>
2019-02-28 14:27:27 -0800

    Reorganize .scss files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_195329) do
+ActiveRecord::Schema.define(version: 2019_02_28_220528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "friend_id"], name: "index_friends_on_user_id_and_friend_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
