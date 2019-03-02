7f4ea75f275a83ca6b84916428ea14994418b50e
marianna mullens <mariannamullens@gmail.com>
2019-03-02 08:58:43 -0800

    Add unstyled friends to Dashboard, fetchUser on FriendsIndex load

--- frontend/components/friends/friends_index.jsx
+++ frontend/components/friends/friends_index.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
 class FriendsIndex extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchUser(this.props.currentUserId);
+  }
+
   render() {
+    const friends = this.props.friends.map( friend => (
+      <FriendsIndexItem 
+        friend={friend}
+        key={friend.id} 
+      />
+    ));
+
     return (
       <div className="friends-index">
+        <header>
+          <h2>Friends</h2>
+          {/* <AddFriend /> */}
+        </header>
+
+        <ul>{friends}</ul>
       </div>
     )
   }
 };
+
+const FriendsIndexItem = (props) => (
+  <li>{props.friend.name}</li>
+)
 
 export default FriendsIndex;
--- frontend/components/friends/friends_index_container.js
+++ frontend/components/friends/friends_index_container.js
@@ -2,13 +2,16 @@ import { connect } from 'react-redux';
 import FriendsIndex from './friends_index';
 import { createFriend } from '../../actions/friends_actions';
 import { selectFriends } from '../../reducers/entities/friends_selector';
+import { fetchUser } from '../../actions/users_actions';
 
 const mapStateToProps = state => ({
   friends: selectFriends(state),
+  currentUserId: state.session.currentUserId,
 });
 
 const mapDispatchToProps = dispatch => ({
   createFriend: friend => dispatch(createFriend(friend)),
+  fetchUser: id => dispatch(fetchUser(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(FriendsIndex);
