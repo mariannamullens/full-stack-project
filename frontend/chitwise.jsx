942b64c0ca2a67306a213a390980ef5adcb6424a
marianna mullens <mariannamullens@gmail.com>
2019-03-04 14:43:13 -0800

    Add bills API util

--- frontend/chitwise.jsx
+++ frontend/chitwise.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-// import { createFriend } from './actions/friends_actions';
-import { login } from './actions/session_actions';
-// import { receiveFriend } from './actions/friends_actions';
-import { selectFriends } from './reducers/entities/friends_selector';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -25,11 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // TOFIX: TEST
     window.getState = store.getState;
-    // window.createFriend = createFriend;
-    window.login = login;
     window.dispatch = store.dispatch;
-    window.selectFriends = selectFriends;
-    // window.receiveFriend = receiveFriend;
   // testing
 
   ReactDOM.render(<Root store={store} />, root)
---
+++ frontend/util/bills_api_util.js
@@ -0,0 +1,29 @@
+export const fetchBill = id => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/bills/${id}`
+  })
+);
+
+export const createBill = bill => (
+  $.ajax({
+    method: 'POST',
+    url: `/api/bills`,
+    data: { bill }
+  })
+);
+
+export const updateBill = bill => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/bills/${bill.id}`,
+    data: { bill }
+  })
+);
+
+export const deleteBill = id => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/bills/${id}`
+  })
+);
