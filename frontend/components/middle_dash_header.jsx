b5d89d2ce70d634849d97f50eb2585e31b148e62
marianna mullens <mariannamullens@gmail.com>
2019-03-04 21:17:16 -0800

    Add styling to MiddleDashHeader

--- frontend/components/middle_dash_header.jsx
+++ frontend/components/middle_dash_header.jsx
@@ -5,10 +5,25 @@ class MiddleDashHeader extends React.Component {
     let { totalBalance, oweAmount, owedAmount } = this.props
     return (
       <div className="middle-dash-header">
-        <h2>Dashboard</h2>
-        <h2>total balance: {totalBalance}</h2>
-        <h2>you owe: {oweAmount}</h2>
-        <h2>you are owed: {owedAmount}</h2>
+        <div className="upper-header">
+          <h2>Dashboard</h2>
+        </div>
+        <div className="aggregates">
+          <div className="aggregate-type">
+            <h3>total balance:</h3> 
+            <h4>{totalBalance}</h4>
+          </div>
+
+          <div className="aggregate-type border">
+            <h3>you owe:</h3> 
+            <h4>{oweAmount}</h4>
+          </div>
+
+          <div className="aggregate-type green">
+            <h3>you are owed:</h3> 
+            <h4>{owedAmount}</h4>
+          </div>
+        </div>
       </div>
     )
   }
--- frontend/components/middle_dash_header_container.js
+++ frontend/components/middle_dash_header_container.js
@@ -1,16 +1,11 @@
 import { connect } from 'react-redux';
 import MiddleDashHeader from './middle_dash_header';
-import { totalBalance, balancePerFriend, oweAmount, owedAmount } from '../reducers/balance_aggregates_selector';
+import { totalBalance, oweAmount, owedAmount } from '../reducers/balance_aggregates_selector';
 
 const mapStateToProps = state => ({
   totalBalance: totalBalance(state),
-  balancePerFriend: balancePerFriend(state),
   oweAmount: oweAmount(state),
   owedAmount: owedAmount(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(MiddleDashHeader);
+export default connect(mapStateToProps)(MiddleDashHeader);
