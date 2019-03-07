d9348260f83867f86907cb47a574a1d7dd296817
marianna mullens <mariannamullens@gmail.com>
2019-03-07 15:30:37 -0800

    Add links to left nav and fix a few style bugs

--- frontend/components/left_nav.jsx
+++ frontend/components/left_nav.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import FriendsIndexContainer from './friends/friends_index_container';
+import { Link } from 'react-router-dom';
 
 class LeftNav extends React.Component {
   render() {
     return (
       <div className="left-nav">
+        <div className="dash-link">
+          <Link to="/dashboard" className="router-link">
+            <img src={window.images.banana} />
+            Dashboard
+          </Link>
+        </div>
+        <div className="expenses-link">
+          <Link to="/all" className="router-link">
+            <i class="fas fa-list-ul"></i>
+            All expenses
+          </Link>
+        </div>
         <FriendsIndexContainer />
       </div>
     )
